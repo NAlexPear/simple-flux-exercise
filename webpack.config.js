9e9353fac7d0bfb93ef2a9a2683e7a1115f8df9f
Alex Pearson <apearson@five-startech.com>
2017-05-17 11:27:28 -0500

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require( "path" );
+var webpack = require( "webpack" );
+
+module.exports = {
+    "output": {
+        "filename": "index.js"
+    },
+    "module": {
+         "rules": [
+             { "parser": { "requireEnsure": false } },
+             {
+                 "test": /\.jsx$/,
+                 "exclude": /node_modules/,
+                 "loader": "babel-loader",
+                 "options": {
+                     "babelrc": false,
+                     "presets": [
+                         [ "es2015", { "modules": false } ],
+                         "react"
+                     ],
+                     "plugins": [
+                         "lodash"
+                     ]
+                 }
+             }
+         ]
+    },
+    "node": {
+        "fs": "empty"
+    },
+    "devServer": {
+        "https": true,
+        "port": 8080
+    },
+    "devtool": "source-maps",
+    "stats": false && {
+        "colors": true,
+        "modules": true,
+        "reasons": true
+    }
+};
